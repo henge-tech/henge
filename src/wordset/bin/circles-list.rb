88b1f957729d4c6f4f788491a972904c3aee16f0
KOSEKI Kengo <koseki@gmail.com>
2016-09-03 10:57:12 +0900

    Update circles list

--- src/wordset/bin/circles-list.rb
+++ src/wordset/bin/circles-list.rb
@@ -5,12 +5,20 @@
 #
 
 dir = File.join(__dir__, '../../../data/circles/')
+require 'yaml'
 
 files = nil
 map = nil
 Dir.chdir(dir) do
   lines = %x{wc -l *.yml}
-  map = Hash[lines.split(/\n/).map {|line| line.strip}.map {|line| token = line.split(/\s/); [token[1], token[0]] }]
+  map = Hash[lines.split(/\n/).map {|line| line.strip}.map {|line| token = line.split(/\s/); [token[1], token[0].to_i] }]
+
+  Dir.glob('*.yml').each do |file|
+    data = YAML.load(File.read(file))
+    if data.length != map[file] || ![8, 12, 16, 20].include?(map[file])
+      puts "ERROR: #{file} #{data.length} #{map[file]}"
+    end
+  end
 end
 
 open(File.join(__dir__, '../../../data/circles.md'), 'w') do |out|
@@ -21,10 +29,10 @@ open(File.join(__dir__, '../../../data/circles.md'), 'w') do |out|
 
   map.keys.sort.each do |key|
     next if key == 'total'
-    if map[key] == '12'
+    if map[key] == 12
       out.puts "|#{key.ljust(16)}|#{"**#{map[key]}**".ljust(16)}|"
     else
-      out.puts "|#{key.ljust(16)}|#{map[key].ljust(16)}|"
+      out.puts "|#{key.ljust(16)}|#{map[key].to_s.ljust(16)}|"
     end
   end
 end
