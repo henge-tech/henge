9929523463f6acedc125766d9d3da9652c5aed0d
KOSEKI Kengo <koseki@gmail.com>
2016-09-03 11:31:09 +0900

    Check duplication

--- src/wordset/bin/check-order.rb
+++ src/wordset/bin/check-order.rb
@@ -6,7 +6,14 @@ Dir.chdir(File.join(__dir__, '../../../data/circles')) do
   Dir.glob('*.yml').each do |file|
     list = YAML.load(File.read(file))
     s1 = list
-    s2 = list.sort_by(&:downcase)
+    s2 = list.sort_by(&:downcase).uniq
+
+    if s1.length != s2.length
+      puts "ERROR: #{file}"
+    end
+    unless [8,12,16,20].include?(s1.length)
+      puts "ERROR: #{file}"
+    end
 
     s1.each.with_index do |w, i|
       if w != s2[i]
