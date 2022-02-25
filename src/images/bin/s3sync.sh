17472c450b89fc780765dcb8228b27eb60dd6ea5
KOSEKI Kengo <koseki@gmail.com>
2022-02-26 00:23:29 +0900

    Add images

--- src/images/bin/s3sync.sh
+++ src/images/bin/s3sync.sh
@@ -2,9 +2,9 @@
 
 set -ex
 
-aws s3 sync --profile $1 ./images s3://henge/words/
-aws s3 sync --profile $1 ./thumbs s3://henge/thumbs/
-aws s3 sync --profile $1 ./thumbs-low1 s3://henge/thumbs-low1/
-aws s3 sync --profile $1 ./thumbs-low2 s3://henge/thumbs-low2/
-aws s3 sync --profile $1 ./thumbs-low3 s3://henge/thumbs-low3/
-aws s3 sync --profile $1 ./circles s3://henge/circles/
+aws s3 sync ./images s3://henge/words/
+aws s3 sync ./thumbs s3://henge/thumbs/
+aws s3 sync ./thumbs-low1 s3://henge/thumbs-low1/
+aws s3 sync ./thumbs-low2 s3://henge/thumbs-low2/
+aws s3 sync ./thumbs-low3 s3://henge/thumbs-low3/
+aws s3 sync ./circles s3://henge/circles/
