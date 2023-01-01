dea2badbd705ceff781e1cc2534eb392360a3780
KOSEKI Kengo <koseki@gmail.com>
2023-01-01 22:05:56 +0900

    Move the sentence data files and switch the format to YAML

--- src/images/bin/s3sync.sh
+++ src/images/bin/s3sync.sh
@@ -2,9 +2,12 @@
 
 set -ex
 
+cd $(dirname $0)/..
+
 aws s3 sync ./images s3://henge/words/
 aws s3 sync ./thumbs s3://henge/thumbs/
 aws s3 sync ./thumbs-low1 s3://henge/thumbs-low1/
 aws s3 sync ./thumbs-low2 s3://henge/thumbs-low2/
 aws s3 sync ./thumbs-low3 s3://henge/thumbs-low3/
 aws s3 sync ./circles s3://henge/circles/
+aws s3 sync ./sentences s3://henge/sentences/
